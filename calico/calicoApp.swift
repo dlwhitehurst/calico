a7654567a5756ff0e16c685f0c3d87a742817ef0
David L. Whitehurst <dlwhitehurst@gmail.com>
2020-12-13 17:50:53 -0500

    Initial Commit

---
+++ calico/calicoApp.swift
@@ -0,0 +1,17 @@
+//
+//  calicoApp.swift
+//  calico
+//
+//  Created by David Whitehurst on 12/13/20.
+//
+
+import SwiftUI
+
+@main
+struct calicoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
